f968df4b2107a72d26141a4c1a9c994d73a43b9f
Scott Arciszewski <scott@resonantcore.net>
2015-03-02 18:00:27 -0500

    Prepare for initial release (0.1.0) that covers `hash_equals()`

--- src/Security.php
+++ src/Security.php
@@ -1,28 +1,75 @@
 <?php
 namespace ResonantCore\PHPFuture;
 
+/**
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2015 Resonant Core, LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 class Security
 {
     /**
-     * Prevent timing attack
+     * Equivalent to hash_equals() in PHP 5.6 
+     * 
      * @param  string $knownString
      * @param  string $userString
      * @return bool
      */
-    public function hash_equals($knownString, $userString)
+    public static function hash_equals($knownString, $userString)
     {
         if (function_exists('\\hash_equals')) {
             return \hash_equals($knownString, $userString);
         }
-        if (strlen($knownString) !== strlen($userString)) {
+        // We have to roll our own
+        $kLen = self::_strlen($knownString);
+        $uLen = self::_strlen($userString);
+        if ($kLen !== $uLen) {
             return false;
         }
-        $len = strlen($knownString);
         $result = 0;
-        for ($i = 0; $i < $len; $i++) {
+        for ($i = 0; $i < $kLen; $i++) {
             $result |= (ord($knownString[$i]) ^ ord($userString[$i]));
         }
         // They are only identical strings if $result is exactly 0...
         return 0 === $result;
+    }
+    
+    /**
+     * Multi-byte-safe string length calculation
+     * 
+     * @param string $str
+     * @return int
+     */
+    private static function _strlen($str)
+    {
+        // Premature optimization: cache the function_exists() result
+        static $exists = null;
+        if ($exists === null) {
+            $exists = \function_exists('\\mb_strlen');
+        }
+        
+        // If it exists, we need to make sure we're using 8bit mode
+        if ($exists) {
+            return \mb_strlen($str, '8bit');
+        }
+        return \strlen($str);
     }
 }
